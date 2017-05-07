6676e8b9eda50288605bc647b070421e723df26f
Pablo Margreff <pmargreff@gmail.com>
2017-05-07 13:06:25 -0300

    feature: refactor messages for a better portability

--- juliavm.sh
+++ juliavm.sh
@@ -3,23 +3,23 @@
 { # this ensures the entire script is downloaded #
 
 # Setup mirror location if not already set
-if [ -z "$JULIAVM_JULIA_REPO" ]; then
-  export JULIAVM_JULIA_REPO="https://github.com/JuliaLang/julia"
-fi
-if [ -z "$JULIAVM_JULIA_AWS" ]; then
-  export JULIAVM_JULIA_AWS="https://julialang.s3.amazonaws.com/bin/linux/"
-fi
-if [ -z "$JULIAVM_WORK_DIR" ]; then
-  export JULIAVM_WORK_DIR
-  JULIAVM_WORK_DIR=$( cd "$( dirname "$0" )" && pwd )
-fi
+export JULIAVM_JULIA_REPO="https://github.com/JuliaLang/julia"
+export JULIAVM_JULIA_AWS="https://julialang.s3.amazonaws.com/bin/linux/"
+export JULIAVM_WORK_DIR
+JULIAVM_WORK_DIR=$( cd "$( dirname "$0" )" && pwd )
 
-juliavm_has(){
-  type "${1-}" > /dev/null 2>&1
+juliavm_echo() {
+  command printf %s\\n "$*" 2>/dev/null || {
+    juliavm_echo() {
+      # shellcheck disable=SC1001
+      \printf %s\\n "$*" # on zsh, `command printf` sometimes fails
+    }
+    juliavm_echo "$@"
+  }
 }
 
 juliavm_ls_remote() {
-  echo "List of versions available for julia language:"
+  juliavm_echo "Versions available for julia language:"
   command git ls-remote -t $JULIAVM_JULIA_REPO | cut -d '/' -f 3 | cut -c 1 --complement |cut -d '^' -f 1
 }
 
@@ -30,15 +30,20 @@ juliavm_install(){
   dists_dir=$(juliavm_get_dist_dir "$1" "$2")
 
   if [ -d "$dists_dir" ]; then
-    echo $dists_dir' already exist'
+    juliavm_echo $dists_dir' already exist'
   else
+    juliavm_echo 'Creating directories ...'
     command mkdir "$dists_dir"
     command cd "$dists_dir"
+    juliavm_echo 'Downlowding files ...'
     command curl -O "$url"
     command cd "$JULIAVM_WORK_DIR"
+    juliavm_echo 'Unzip files ...'
     command tar -xvzf "$dists_dir"/"$file".tar.gz -C "$dists_dir" --strip-components=1
+    juliavm_echo 'Cleaning ...'
     command rm "$dists_dir"/"$file".tar.gz
   fi
+  juliavm_echo "Julia "$1" installed!"
   juliavm_use $1
 }
 
@@ -51,8 +56,8 @@ juliavm_use(){
     EXEC_PATH="$JULIAVM_WORK_DIR/dists/$1/bin/julia"
   fi
   sed -i /'alias julia='/d  ~/.bashrc
-  echo "You're now using Julia $1$2"
-  echo "alias julia='$EXEC_PATH'" >> ~/.bashrc && exec bash
+  juliavm_echo "You're using Julia $1$2"
+  juliavm_echo "alias julia='$EXEC_PATH'" >> ~/.bashrc && exec bash
 }
 
 juliavm_ls(){
@@ -72,7 +77,7 @@ juliavm_version_is_available_locale(){
   if [ -d "$VERSION_DIR" ]; then
     return 0
   else
-    echo "Version isn't available, all version ready for use are: "
+    juliavm_echo "Version isn't available, all version ready for use are: "
     juliavm_ls
     return 1
   fi
@@ -84,8 +89,8 @@ juliavm_version_is_available_remote(){
   if eval "curl --output /dev/null --silent --head --fail \"$url\""; then
     return 0
   else
-    echo "$url isn't available"
-    echo "You can list all available versions with ls-remote"
+    juliavm_echo "$url isn't available"
+    juliavm_echo "You can list all available versions with ls-remote"
     return 1
   fi
 }
@@ -94,13 +99,13 @@ juliavm_version_is_available_remote(){
 juliavm_get_file_name(){
   if [[ "$2" == '-x86' ]]; then
     file='julia-'$1'-linux-i686'
-    echo $file
+    juliavm_echo $file
   elif [[ "$2" == '-x64' ]]; then
     file='julia-'$1'-linux-x86_64'
-    echo $file
+    juliavm_echo $file
   else
     file='julia-'$1'-linux-x86_64'
-    echo $file
+    juliavm_echo $file
   fi
 }
 
@@ -111,28 +116,28 @@ juliavm_get_download_url(){
   if [[ "$2" == '-x86' ]]; then
     arch='x86/'
     url=$JULIAVM_JULIA_AWS$arch$major$file'.tar.gz'
-    echo $url
+    juliavm_echo $url
   elif [[ "$2" == '-x64' ]]; then
     arch='x64/'
     url=$JULIAVM_JULIA_AWS$arch$major$file'.tar.gz'
-    echo $url
+    juliavm_echo $url
   else
     arch='x64/'
     url=$JULIAVM_JULIA_AWS$arch$major$file'.tar.gz'
-    echo $url
+    juliavm_echo $url
   fi
 }
 
 juliavm_get_dist_dir(){
   if [[ "$2" == '-x86' ]]; then
     dist_dir=$JULIAVM_WORK_DIR'/dists/'$1$2
-    echo $dist_dir
+    juliavm_echo $dist_dir
   elif [[ "$2" == '-x64' ]]; then
     dist_dir=$JULIAVM_WORK_DIR'/dists/'$1
-    echo $dist_dir
+    juliavm_echo $dist_dir
   else
     dist_dir=$JULIAVM_WORK_DIR'/dists/'$1
-    echo $dist_dir
+    juliavm_echo $dist_dir
   fi
 }
 
@@ -144,34 +149,37 @@ juliavm_update(){
 
 
 juliavm_help() {
-  echo "  install x.y.z     install x.y.x version [ARCHITECTURE]"
-  echo "  use x.y.z         use x.y.x version [ARCHITECTURE]"
-  echo "  ls-remote         list all remote versions"
-  echo "  ls                list all locale versions"
-  echo "  update            update juliavm with latest resources"
-  echo "  uninstall         uninstall juliavm and all julia versions downloaded inside juliavm"
-  echo "      --hard        uninstall all Julia packages, if isn't passed with uninstall command. soft uninstall will be used"
-  echo " "
-  echo "  help              list all commands"
-  echo " "
-  echo "ARCHITECTURE options (if you don't pass unix 64 bits will be used):"
-  echo "  -x64    unix 64 bits"
-  echo "  -x86    unix 32 bits"
+  juliavm_echo "  install x.y.z     install x.y.x version [ARCHITECTURE]"
+  juliavm_echo "  use x.y.z         use x.y.x version [ARCHITECTURE]"
+  juliavm_echo "  ls-remote         list all remote versions"
+  juliavm_echo "  ls                list all locale versions"
+  juliavm_echo "  update            update juliavm with latest resources"
+  juliavm_echo "  uninstall         uninstall juliavm and all julia versions downloaded inside juliavm"
+  juliavm_echo "      --hard        uninstall all Julia packages, if isn't passed with uninstall command. soft uninstall will be used"
+  juliavm_echo " "
+  juliavm_echo "  help              list all commands"
+  juliavm_echo " "
+  juliavm_echo " ARCHITECTURE options (if you don't pass unix 64 bits will be used):"
+  juliavm_echo "  -x64    unix 64 bits"
+  juliavm_echo "  -x86    unix 32 bits"
 }
 
 juliavm_uninstall(){
   echo 'Uninstalling ...'
   if  [[ "$1" == '--hard' ]]; then
-    echo 'Deleting all Julia packages ...'
+    juliavm_echo 'Deleting all Julia packages ...'
     juliavm_uninstall_packages
   else
-    echo 'Julia packages will be kept ...'
+    juliavm_echo 'Julia packages will be kept ...'
   fi
 
   DIR=$( cd "$( dirname "$0" )" && pwd )
   sed -i /'alias julia='/d  ~/.bashrc
   sed -i /'alias juliavm='/d  ~/.bashrc
   command rm -r ~/.juliavm
+  command unset JULIAVM_JULIA_REPO
+  command unset JULIAVM_JULIA_AWS
+  command unset JULIAVM_WORK_DIR
 }
 
 juliavm_uninstall_packages(){
@@ -196,10 +204,10 @@ elif [[ "$1" == 'update' ]]; then
 elif [[ "$1" == 'uninstall' ]]; then
   juliavm_uninstall $2
 elif [[ "$1" == *"help"* ]]; then
-  echo "Commands available are: "
+  juliavm_echo "Commands available are: "
   juliavm_help
 else
-  echo "Command not found, commands available are: "
+  juliavm_echo "Command not found, commands available are: "
   juliavm_help
 fi
 
