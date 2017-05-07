f683744a4f0a9c847fc747d964b7a97e81e51ed6
Pablo Margreff <pmargreff@gmail.com>
2017-05-07 13:08:19 -0300

    doc: add some messages for installation process

--- install.sh
+++ install.sh
@@ -4,9 +4,22 @@
   CURRENT_DIR=$( cd "$( dirname "$0" )" && pwd )
   INSTALL_DIR="$HOME/.juliavm"
 
+  juliavm_echo() {
+    command printf %s\\n "$*" 2>/dev/null || {
+      juliavm_echo() {
+        # shellcheck disable=SC1001
+        \printf %s\\n "$*" # on zsh, `command printf` sometimes fails
+      }
+      juliavm_echo "$@"
+    }
+  }
+
   juliavm_install(){
+    juliavm_echo "Creating directories structure ..."
     juliavm_create_directories
+    juliavm_echo "Moving files ..."
     juliavm_copy_files
+    juliavm_echo "Juliavm successfully installed!!"
   }
 
   juliavm_create_directories(){
